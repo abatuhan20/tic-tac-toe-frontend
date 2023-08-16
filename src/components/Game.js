149204b37fb4c76e50288015cb63e0118fd49200
Arda AYDIN <92793670+abatuhan20@users.noreply.github.com>
2023-08-16 21:20:31 +0300

    stream-chat-react + css + new components

---
+++ src/components/Game.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Channel } from 'stream-chat-react'
+import { StreamChat } from 'stream-chat'
+import Board from './Board'
+
+const Game = ({channel}) => {
+    const  [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2);
+    // user.watching.start user o sayfaya ne zaman bağlanırsa
+    // channel.on fonksiyonu 2 kişide de sayfanın yenilenmesini sağlıyor.
+    channel.on("user.watching.start", (event) => {
+        setPlayersJoined(event.watcher_count === 2)
+    });
+    // channel.state.watcher_count bağlantı sayısını trackliyor
+    // Oyuncuların bağlanıp bağlanmadığına bu kısımda bakabiliyorum.
+    if(!playersJoined) {
+        return <div>Waiting for other player to join...</div>
+    }
+  return (
+    <div className='gameContainer'>
+        <Board />
+        {/* CHAT */}
+        {/* LEAVE GAME BUTTON */}
+    </div>
+  )
+}
+
+export default Game
